41782163d94248588b9b8ac4f29a14933901e751
Mario Rufo Ariza <mariojra6@gmail.com>
2022-11-25 16:23:05 +0100

    Archivos actualizados

---
+++ examenPHP/Soporte.php
@@ -0,0 +1,67 @@
+<?php 
+
+class Soporte {
+    public string $titulo;
+    private string $numero;
+    private float $precio;
+
+    const IVA = 0.21;
+
+    public function __construct($titulo, $numero, $precio)
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+    }
+
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo)
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+ 
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function setNumero($numero)
+    {
+        $this->numero = $numero;
+
+        return $this;
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set the value of sueldo
+     *
+     * @return  self
+     */ 
+    public function setPrecio($precio)
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getPrecioConIVA() {  
+        return $this->precio + ($this->precio * self::IVA);
+    }
+
+    public function muestraResumen() : void {
+        echo '<br><br><b>Resumen:</b><br>'.'Título = '.$this->titulo.', Número = '.$this->numero.', Precio = '.$this->precio.', Precio con IVA = '.$this->getPrecioConIVA();
+    }
+}
+
+?>
